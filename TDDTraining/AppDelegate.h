582e933dbfbe09331285440b197a28af73f06aaa
Robert Mißbach <mail@robert-missbach.com>
2015-03-07 10:12:17 +0100

    Initial Commit

---
+++ TDDTraining/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TDDTraining
+//
+//  Created by Robert Mißbach on 07.03.15.
+//  Copyright (c) 2015 Robert Mißbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TDDTraining/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TDDTraining
+//
+//  Created by Robert Mißbach on 07.03.15.
+//  Copyright (c) 2015 Robert Mißbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
