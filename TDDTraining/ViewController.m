64e12f3a01d3380aaee3958524fdbe7d318d8ed5
Robert Mißbach <robert.missbach@holidaycheck.com>
2015-03-08 18:34:01 +0100

    UI for roman number converter

--- TDDTraining/ViewController.m
+++ TDDTraining/ViewController.m
@@ -7,17 +7,32 @@
 //
 
 #import "ViewController.h"
+#import "NumberConverter.h"
+
 
 @interface ViewController ()
+
+/// The text field which will display the roman number
+@property (weak, nonatomic) IBOutlet UITextField *romanNumberTextField;
 
 @end
 
 
 @implementation ViewController
 
-- (void)viewDidLoad
+- (IBAction)editingDidChange:(UITextField *)sender
 {
-    [super viewDidLoad];
+    NSUInteger newValue = sender.text.integerValue;
+    
+    if (newValue == 0)
+    {
+        self.romanNumberTextField.text = @"Invalid number!";
+        return;
+    }
+    
+    NSString *romanNumber = [NumberConverter romanNumberForArabicNumber:newValue];
+    self.romanNumberTextField.text = romanNumber;
 }
+
 
 @end
